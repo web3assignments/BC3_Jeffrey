90fa577a8873b4224214e055aa9ab8d4b30abb10
Jeffrey <loafoe.jeffrey@gmail.com>
2020-12-11 18:32:02 +0100

    changed homepage

---
+++ PD-07/ChainsGathering/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BC3_Jeffrey/PD-07/ChainsGathering/public/precache-manifest.f4be47c7bf4ad5c69e9b802aa30eca77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BC3_Jeffrey/PD-07/ChainsGathering/public/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
---
+++ PD-07/ChainsGathering/public/precache-manifest.f4be47c7bf4ad5c69e9b802aa30eca77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97bf85d63ea050e2757fd660ce31790",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/index.html"
+  },
+  {
+    "revision": "03a2ddd9e1b6f609f33b",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "6911cf9d003a8e658e8f",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/css/main.b6ea7c43.chunk.css"
+  },
+  {
+    "revision": "03a2ddd9e1b6f609f33b",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/js/2.1447a244.chunk.js"
+  },
+  {
+    "revision": "edf0e9a61fe213df5b1c371436d23575",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/js/2.1447a244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6911cf9d003a8e658e8f",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/js/main.a1c52ce7.chunk.js"
+  },
+  {
+    "revision": "eceb154b36472f71d506",
+    "url": "/BC3_Jeffrey/PD-07/ChainsGathering/public/static/js/runtime-main.8957195a.js"
+  }
+]);
